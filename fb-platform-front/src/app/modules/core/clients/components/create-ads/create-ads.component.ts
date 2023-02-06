9fb48fd27b70bf6feee2fb67858900059a7d7b3f
Abdelkarim Nourti <kariimnourti15@gmail.com>
2023-02-06 21:03:43 +0100

    feature | WIP create ads

--- fb-platform-front/src/app/modules/core/clients/components/create-ads/create-ads.component.ts
+++ fb-platform-front/src/app/modules/core/clients/components/create-ads/create-ads.component.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {FormArrayName, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {FacebookService} from "../../../services/facebook.service";
 import {Campaign} from "../../../../../models/campaign";
 import {DynamicDialogRef} from "primeng/dynamicdialog";
 import {MessageService} from "primeng/api";
-import {$E} from "@angular/compiler/src/chars";
 import {FileUpload} from "primeng/fileupload";
 
 @Component({
@@ -15,10 +14,10 @@ import {FileUpload} from "primeng/fileupload";
 export class CreateAdsComponent implements OnInit {
   valid = true;
   beanAds = new FormGroup({
-    name: new FormControl('',[Validators.required]),
-    campaign: new FormControl('',[Validators.required]),
-    budget: new FormControl(0,[Validators.required]),
-   });
+    name: new FormControl('', [Validators.required]),
+    campaign: new FormControl('', [Validators.required]),
+    budget: new FormControl(0, [Validators.required]),
+  });
   /*
     title : new FormControl([],[Validators.required]),
 */
@@ -26,7 +25,7 @@ export class CreateAdsComponent implements OnInit {
   compaigns: Campaign[] = [];
   uploadedFiles: any[] = [];
   uploadedFilesVideo: any[] = [];
-  @ViewChild('upload') upload !:  FileUpload
+  @ViewChild('upload') upload !: FileUpload
 
   constructor(private facebookApi: FacebookService, private dialogRef: DynamicDialogRef, public messageService: MessageService) {
   }
@@ -41,11 +40,23 @@ export class CreateAdsComponent implements OnInit {
   onSubmit(): void {
     this.valid = this.beanAds.valid;
     console.log(this.beanAds.value);
+    this.facebookApi.getAdSetById(this.beanAds.value.campaign.ads.data[0].adset_id).subscribe(oldAdSet => {
+      this.facebookApi.duplicateAdSets(oldAdSet).subscribe(newAdSet => {
+        console.log("duplicateAdSets",newAdSet);
+        this.facebookApi.createAdCreative().subscribe(creativeAd => {
+          console.log("adCreative has been created",creativeAd);
+          this.facebookApi.createAd(newAdSet.id, creativeAd.id).subscribe(data => {
+            console.log('ad has been created', data);
+          })
+        })
+      })
+    })
+
     console.log(this.beanAds.valid);
   }
 
   onchangeCompaigns($event: any): void {
-    console.log($event);
+    console.log('compaign values', $event);
   }
 
   close(): void {
@@ -55,11 +66,11 @@ export class CreateAdsComponent implements OnInit {
 
   onUpload(event: any): void {
     let sommelength = event.files.length + this.uploadedFiles.length
-    if (event.files.length <= 5 && sommelength <= 5 ) {
+    if (event.files.length <= 5 && sommelength <= 5) {
       for (let file of event.files) {
-      this.uploadedFiles.push(file);
-    }
-  }else {
+        this.uploadedFiles.push(file);
+      }
+    } else {
       this.messageService.add(
         {
           severity: 'error',
@@ -71,12 +82,12 @@ export class CreateAdsComponent implements OnInit {
 
   onUploadVideo(event: any): void {
     let sommelength = event.files.length + this.uploadedFilesVideo.length
-    if (event.files.length <= 5 && sommelength <= 5 ) {
+    if (event.files.length <= 5 && sommelength <= 5) {
       for (let file of event.files) {
         this.uploadedFilesVideo.push(file);
       }
     } else {
-       this.messageService.add(
+      this.messageService.add(
         {
           severity: 'error',
           summary: 'Error',
